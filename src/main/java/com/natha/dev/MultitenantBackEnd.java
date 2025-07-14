896b8473c6e53a4e512fe68ca8335c156ed393cb
Nathabijou <nathabijou@gmail.com>
2025-07-14 11:40:17 -0400

    Repair properties

--- src/main/java/com/natha/dev/MultitenantBackEnd.java
+++ src/main/java/com/natha/dev/MultitenantBackEnd.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Vsl {
+public class MultitenantBackEnd {
 
     public static void main(String[] args) {
-        SpringApplication.run(Vsl.class, args);
+        SpringApplication.run(MultitenantBackEnd.class, args);
     }
 
 }
